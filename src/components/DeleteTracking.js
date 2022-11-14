1eafee5203072030a262b96fef667fbe4931099b
jamesdjetpack <jamesd@jetpackshipping.com>
2022-11-14 15:49:30 -0500

    EditedAndCleanedABit

--- src/components/DeleteTracking.js
+++ src/components/DeleteTracking.js
@@ -11,6 +11,8 @@ export default function DeleteTracking () {
     const [trackingList, setTrackingList] = useState();
     
 
+    // TODO CLEAN UP AND PLACE CONSTANTS HERE
+
     // need a function to fetch all of the cancelled orders
     // Need a map of all of the cancelled orders with tracking numbers 
     // need to go through each one and cancel the order. 
@@ -25,7 +27,6 @@ export default function DeleteTracking () {
     // Note: the list of orders may be in batches of 500
     const retrieveCanceledOrders = async () => {
         try{
-
             //### Credentials here are temporary. Change to .env variables later. 
             const MERCHANT = process.env.REACT_APP_TEST_MERCHANT
             const API_KEY = process.env.REACT_APP_TEST_API_KEY
@@ -45,7 +46,7 @@ export default function DeleteTracking () {
             // Had to modify and use search because search_read had a 500 limit for shipment.out
 var config = {
   method: 'put',
-  url: 'https://jetpack-sandbox.fulfil.io/api/v2/model/stock.shipment.out/search',
+  url: 'https://'+MERCHANT+'.fulfil.io/api/v2/model/stock.shipment.out/search',
   headers: { 
     'x-api-key': API_KEY, 
     'Content-Type': 'text/plain', 
@@ -65,30 +66,6 @@ const request = axios(config)
 .catch(function (error) {
     console.log(error);
   });
-            // const request = await axios.put("https://"+merchant+".fulfil.io/api/v2/model/stock.shipment.out/search_read",
-            //         filter
-            //     , 
-            //     headersConfig).then((response)=> {
-            //         console.log('The response was the following'+ response);
-            //         console.log("")
-            //        // I should check if there was an error response for the call and cancel.  
-            //        // ex while (response!=happyState) {send again, then check happy state, make happystate false?}
-            //        if (response.data()!= []) {
-            //         console.log('response',response.data);
-            //         console.log('response was a success'); 
-            //         }
-            //         else{
-            //         console.log('response', response.data); 
-            //         console.log('the response was not a success'); 
-            //         //failstate = true; 
-            //         }
-                                
-            //         // handle failure. 
-            //     }).catch((error)=> {
-            //         alert('errors', error.response);
-            //         console.log("error response is below, followed by the error object");
-        
-            //     });
             console.log(responseSee)
             await console.log(request)
                 const styles = {
@@ -118,36 +95,29 @@ const request = axios(config)
 
         let resultArray = [];
         let safeArray = [...listCanceledOrders]
-
+        const MERCHANT = process.env.REACT_APP_MERCHANT
+        const API_KEY = process.env.REACT_APP_API_KEY
         //const copy = array.map(object => ({ ...object }))
         //or spread =. I think spread will clone. 
         // should test
 
 
         const total = safeArray.length; 
-        //let chunks = 1; 
-        //chunks = Math.floor(total/500);
         while(safeArray.length>0){ // use chunk if this doesnt work. 
             resultArray.push(safeArray.splice(0,500));
             //total = total-1; 
         }
-        // if(safeArray.length>1){
-        //     resultArray.push(safeArray)
-        // }
-        //console.log(resultArray)
-        // make the call 
 
         
         let output = [];
-        let output2 = [];
 
 
         // forgot about foreach async snap . 
         await resultArray.forEach( element =>{
         try{
             //### Credentials here are temporary. Change to .env variables later. 
-            const merchant = process.env.REACT_APP_TEST_MERCHANT
-            const API_KEY = process.env.REACT_APP_TEST_API_KEY
+            const MERCHANT = process.env.REACT_APP_MERCHANT
+            const API_KEY = process.env.REACT_APP_API_KEY
             
             // Do not leave this here etc. ####
             const headersConfig = {
@@ -174,26 +144,10 @@ const request = axios(config)
                   console.log("testing print")
                   console.log(test)
                 var data = test
-                // JSON.stringify({
-                //     "filters": [
-                //       [
-                //         "id",
-                //         "in",
-                //         307938
-                //       ]
-                //     ],
-                //     "fields": [
-                //       "id",
-                //       "tracking_number",
-                //       "rec_name",
-                //       "state"
-                //     ]
-                //   });
-
             // Had to modify and use search because search_read had a 500 limit for shipment.out
             var config = {
               method: 'put',
-              url: 'https://jetpack-sandbox.fulfil.io/api/v2/model/stock.shipment.out/search_read',
+              url: 'https://'+MERCHANT+'.fulfil.io/api/v2/model/stock.shipment.out/search_read',
               headers: { 
                 'x-api-key': API_KEY, 
                 'Content-Type': 'text/plain', 
@@ -225,15 +179,6 @@ const request = axios(config)
             console.log(err); 
         }
         })
-        // RawTest.forEach(element => {
-        //     //console.log(element)
-        //     if(element.tracking_number != null){
-        //         output.push(element);
-        //         output2.push(element.tracking_number)
-        //     }
-        // });
-        // console.log(output)
-        // console.log(output2)
         setTrackingList(output);
         console.log("the list of tracking number ids are the following")
         console.log(trackingList)
@@ -250,7 +195,8 @@ const request = axios(config)
     }
 
     const submitListTest  = () => {
-      
+      const MERCHANT = process.env.REACT_APP_MERCHANT
+      const API_KEY = process.env.REACT_APP_API_KEY
       var total = 0; 
       var index1 = 0; 
 
@@ -259,11 +205,6 @@ const request = axios(config)
         var index2 = 0;
         while (index2 < trackingList[index1].length) {
           var submitString = trackingList[index1][index2]
-
-          // call deletion
-          //testDeletionOne(trackingList[0][0])
-          //testDeletionOne(submitString)
-          //console.log(submitString)
           index2 = index2+1; 
           total = total +1;
 
@@ -276,13 +217,14 @@ const request = axios(config)
 
 
     const testDeletionOne = (input)=>{
+      const MERCHANT = process.env.REACT_APP_MERCHANT
+      const API_KEY = process.env.REACT_APP_API_KEY
     var data = '';     
       var config = {
         method: 'delete',
-        url: 'https://jetpack-sandbox.fulfil.io/api/v2/model/shipment.tracking/'+input,
+        url: 'https://'+MERCHANT+'.fulfil.io/api/v2/model/shipment.tracking/'+input,
         headers: { 
-          'x-api-key': '49101de129a34ce79adf7ec2c9ae7f5d', 
-          //'Cookie': 'session=eyJfcGVybWFuZW50Ijp0cnVlfQ.Y26vrA.hS5pj3AunytXg36tZdYexbKnm9k'
+          'x-api-key': API_KEY, 
         },
         data : data
       };
